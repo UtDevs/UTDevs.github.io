7d2949053e4fcac4aff1286204e670279a7325a7
UtDevs <49181476+UtDevs@users.noreply.github.com>
2020-10-10 11:39:33 +0300

    Change Back-end URL

--- axios-auth.js
+++ axios-auth.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-      baseURL: 'https://qcommunicator.herokuapp.com/' // online
+      baseURL: 'http://ec2-3-133-102-228.us-east-2.compute.amazonaws.com:8090/communication/' // online
+      //baseURL: 'https://qcommunicator.herokuapp.com/' // online
       // baseURL: 'http://192.168.1.113:8080/' // testing
 })
 // const token = localStorage.getItem('token')
