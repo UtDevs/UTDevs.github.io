e6cc25a5ec030fc50b02e8c1cc3266eca6672f17
UtDevs <49181476+UtDevs@users.noreply.github.com>
2020-11-06 15:21:33 +0200

    heroku

--- axios-auth.js
+++ axios-auth.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const instance = axios.create({
-
+      //baseURL: 'http://ec2-3-133-102-228.us-east-2.compute.amazonaws.com:8090/communication/' // online
       baseURL: 'https://qcommunicator.herokuapp.com/' // online
       // baseURL: 'http://192.168.1.113:8080/' // testing
-            //baseURL: 'http://ec2-3-133-102-228.us-east-2.compute.amazonaws.com:8090/communication/' // online
 })
 // const token = localStorage.getItem('token')
 // instance.defaults.headers.common['Authorization'] = 'Bearer ' + token
